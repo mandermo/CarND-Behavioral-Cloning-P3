b4cac540e7b11bef6516048f39facf1ffc1b96c8
Magnus Andermo <mandermo@kth.se>
2017-08-23 09:44:58 +0700

    Architecture as Udacity Training the Network video
    Swaying around on road using whole road, later getting stuck.

---
+++ model.py
@@ -0,0 +1,41 @@
+import csv
+import cv2
+import numpy as np
+import os
+
+lines = []
+with open('udacity-my-driving-data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+lines = lines[1:]
+print('num frames: {}'.format(len(lines)))
+images = []
+measurements = []
+for line in lines[0:640]:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = os.path.join('udacity-my-driving-data', 'IMG', filename)
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+print('image shape: {}'.format(image.shape))
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
