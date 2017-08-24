f86a24aaa061fd1134b89c872c9283d0ec6c813b
Magnus Andermo <mandermo@kth.se>
2017-08-24 09:50:01 +0700

    Using generators to feed samples

--- model.py
+++ model.py
@@ -2,30 +2,56 @@ import csv
 import cv2
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
+import sklearn
 
-lines = []
-with open('udacity-my-driving-data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+def read_lines_from_driving_log(path):
+    lines = []
+    with open(path) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+    # First line contains the name of the columns for me. If it just for some
+    # library versions, then it is anyway ok to skip first frame.
+    lines = lines[1:]
 
-lines = lines[1:]
+    return lines
+
+
+def generator_from_lines(lines, batch_size=32):
+    num_samples = len(lines)
+    while True:
+        lines = sklearn.utils.shuffle(lines)
+        for offset in range(0, num_samples, batch_size):
+            batch_lines = lines[offset:offset+batch_size]
+
+            yield lines_to_X_and_y(batch_lines)
+
+
+# Convert a set of lines from a driving log to datas and expected values.
+def lines_to_X_and_y(batch_lines):
+    images = []
+    measurements = []
+    for line in batch_lines:
+        source_path = line[0]
+        filename = source_path.split('/')[-1]
+        current_path = os.path.join('udacity-my-driving-data', 'IMG', filename)
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        measurements.append(measurement)
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+    return X_train, y_train
+
+
+lines = read_lines_from_driving_log('udacity-my-driving-data/driving_log.csv')
+train_lines, validation_lines = train_test_split(lines, test_size=0.2)
+
 print('num frames: {}'.format(len(lines)))
-images = []
-measurements = []
-for line in lines[0:640]:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = os.path.join('udacity-my-driving-data', 'IMG', filename)
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
 
-print('image shape: {}'.format(image.shape))
-
-X_train = np.array(images)
-y_train = np.array(measurements)
+train_generator = generator_from_lines(train_lines)
+validation_generator = generator_from_lines(validation_lines)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
@@ -36,6 +62,9 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit_generator(
+        train_generator, samples_per_epoch=len(train_lines),
+        validation_data=validation_generator,
+        nb_val_samples=len(validation_lines), nb_epoch=7)
 
 model.save('model.h5')
