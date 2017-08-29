78ed8dc086e86fd863d3c866a87c446c8e3e611a
Magnus Andermo <mandermo@kth.se>
2017-08-29 14:39:35 +0800

    Support for multiple drive logs and random flip

--- model.py
+++ model.py
@@ -5,57 +5,94 @@ import os
 from sklearn.model_selection import train_test_split
 import sklearn
 
-def read_lines_from_driving_log(path):
-    lines = []
-    with open(path) as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
-    # First line contains the name of the columns for me. If it just for some
-    # library versions, then it is anyway ok to skip first frame.
-    lines = lines[1:]
 
-    return lines
+class DrivingLogLine:
+    def __init__(self, line, img_dir_path):
+        self.line = line
+        #self.drivelogpath = drivelogpath
+        self.img_dir_path = img_dir_path
+
+    def get_img_path_from_column(self, column):
+        source_path = self.line[column]
+        filename = os.path.basename(source_path) #source_path.split('/')[-1]
+        current_path = os.path.join(self.img_dir_path, filename)
+        return current_path
 
 
-def generator_from_lines(lines, batch_size=32):
+    @property
+    def center_img_path(self):
+        return self.get_img_path_from_column(0)
+
+
+    @property
+    def angle(self):
+        return float(self.line[3])
+
+
+    @classmethod
+    def read_lines_from_driving_log(cls, driving_log_path):
+        lines = []
+        with open(driving_log_path) as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lines.append(line)
+        # First line contains the name of the columns for me. If it just for some
+        # library versions, then it is anyway ok to skip first frame.
+        lines = lines[1:]
+
+        img_dir_path = os.path.join(os.path.dirname(driving_log_path), 'IMG')
+        lines = [cls(line, img_dir_path) for line in lines]
+
+        return lines
+
+
+def generator_from_lines(lines, augment, batch_size=32):
     num_samples = len(lines)
     while True:
         lines = sklearn.utils.shuffle(lines)
         for offset in range(0, num_samples, batch_size):
             batch_lines = lines[offset:offset+batch_size]
 
-            yield lines_to_X_and_y(batch_lines)
+            X,y = zip(*[make_sample_from_line(line, augment) for line in
+                batch_lines])
+            yield np.array(X),np.array(y)
 
 
-# Convert a set of lines from a driving log to datas and expected values.
-def lines_to_X_and_y(batch_lines):
-    images = []
-    measurements = []
-    for line in batch_lines:
-        source_path = line[0]
-        filename = source_path.split('/')[-1]
-        current_path = os.path.join('udacity-my-driving-data', 'IMG', filename)
-        image = cv2.imread(current_path)
-        angle = float(line[3])
-        images.append(image)
-        measurements.append(angle)
+def make_sample_from_line(line, augment):
+    img = cv2.imread(line.center_img_path)
+    angle = line.angle
 
-        # Add vertical flip
-        images.append(cv2.flip(image,1))
-        measurements.append(-angle)
-    X_train = np.array(images)
-    y_train = np.array(measurements)
-    return X_train, y_train
+    # Cropp 70 from top and 25 from bottom
+    cropped = img[70:-25,:,:]
+
+    # Approx normalize
+    img = img / 255.0
+    img -= 0.5
+
+    if augment:
+        if np.random.choice([False, True]):
+            img = cv2.flip(img,1)
+    
+    return (img,angle)
 
 
-lines = read_lines_from_driving_log('udacity-my-driving-data/driving_log.csv')
+def read_all_driving_logs(driving_log_paths):
+    return list(sum(
+        (DrivingLogLine.read_lines_from_driving_log(path)
+            for path in driving_log_paths)
+        , []))
+
+
+driving_log_paths = ['udacity-my-driving-data/driving_log.csv']
+
+lines = read_all_driving_logs(driving_log_paths)
+
 train_lines, validation_lines = train_test_split(lines, test_size=0.2)
 
 print('num frames: {}'.format(len(lines)))
 
-train_generator = generator_from_lines(train_lines)
-validation_generator = generator_from_lines(validation_lines)
+train_generator = generator_from_lines(train_lines, True)
+validation_generator = generator_from_lines(validation_lines, False)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
